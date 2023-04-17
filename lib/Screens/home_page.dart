87df102bef0797ebeb771dc457035f3fe805e932
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-04-18 00:08:01 +0530

    -home screen

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -3,12 +3,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:nari_connect/Screens/auth_page.dart';
 import 'package:nari_connect/Screens/search_page.dart';
 import 'package:nari_connect/main.dart';
 
 import '../helper/helper_function.dart';
 import '../services/auth_service.dart';
 import '../services/database_service.dart';
+import '../widgets/widgets.dart';
+import 'profile_page.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -86,13 +89,13 @@ class _HomePageState extends State<HomePage> {
                     MaterialPageRoute(builder: (context) => SearchPage()));
               },
               icon: const Icon(Icons.search)),
-          IconButton(
-            onPressed: signUserOut,
-            icon: const Icon(
-              Icons.logout,
-              color: Colors.white,
-            ),
-          ),
+          // IconButton(
+          //   onPressed: signUserOut,
+          //   icon: const Icon(
+          //     Icons.logout,
+          //     color: Colors.white,
+          //   ),
+          // ),
         ],
         elevation: 0,
         centerTitle: true,
@@ -104,62 +107,103 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 0),
-          children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Colors.pink,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 50.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      user.displayName!,
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const Icon(
-                      Icons.account_circle,
-                      size: 50,
-                      color: Colors.white,
-                    ),
-                    const SizedBox(
-                      width: 0,
-                    ),
-                  ],
-                ),
-              ),
+          child: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 50),
+        children: <Widget>[
+          Icon(
+            Icons.account_circle,
+            size: 150,
+            color: Colors.grey[700],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Text(
+            userName,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Divider(
+            height: 2,
+          ),
+          ListTile(
+            onTap: () {},
+            selectedColor: Theme.of(context).primaryColor,
+            selected: true,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            leading: const Icon(Icons.group),
+            title: const Text(
+              "Groups",
+              style: TextStyle(color: Colors.black),
             ),
-            ListTile(
-              title: const Text('Home'),
-              onTap: () {
-                Navigator.pop(context);
-              },
+          ),
+          ListTile(
+            onTap: () {
+              nextScreen(
+                  context,
+                  const ProfilePage(
+                      // userName: userName,
+                      // email: email,
+                      ));
+            },
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            leading: const Icon(Icons.group),
+            title: const Text(
+              "Profile",
+              style: TextStyle(color: Colors.black),
             ),
-            ListTile(
-              title: const Text('Search'),
-              onTap: () {
-                navigatorKey.currentState!.push(
-                  MaterialPageRoute(
-                    builder: (context) => SearchPage(),
-                  ),
-                );
-                // Navigator.push(context,
-                //     MaterialPageRoute(builder: (context) => SearchPage()));
-              },
+          ),
+          ListTile(
+            onTap: () async {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Logout"),
+                      content: const Text("Are you sure you want to logout?"),
+                      actions: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(
+                            Icons.cancel,
+                            color: Colors.red,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            signUserOut();
+                            Navigator.of(context).pushAndRemoveUntil(
+                                MaterialPageRoute(
+                                    builder: (context) => const AuthPage()),
+                                (route) => false);
+                          },
+                          icon: const Icon(
+                            Icons.done,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text(
+              "Logout",
+              style: TextStyle(color: Colors.black),
             ),
-            ListTile(
-              title: const Text('Logout'),
-              onTap: signUserOut,
-            ),
-          ],
-        ),
-      ),
+          )
+        ],
+      )),
       body: Center(
         child: Text(
           'Logged in as : ' + user.email!,
